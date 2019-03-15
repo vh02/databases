27d4b014d0325815cf4367c23000d86c680a0a90
Hack Reactor Student <student@hackreactor.com>
2019-03-14 17:19:28 -0700

    Clean code

--- server/schema.sql
+++ server/schema.sql
@@ -20,11 +20,11 @@ id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 text VARCHAR(140) NOT NULL,
 usernameid VARCHAR(16),
 roomnameid VARCHAR(16)
--- FOREIGN KEY (usernameid) REFERENCES users(id),
--- FOREIGN KEY (roomnameid) REFERENCES rooms(id)
 );
--- ALTER TABLE messages ADD FOREIGN KEY (usernameid) REFERENCES users(id);
--- ALTER TABLE messages ADD FOREIGN KEY (roomnameid) REFERENCES rooms(id);
+-- ALTER TABLE messages ADD FOREIGN KEY (usernameid) REFERENCES `users`(`id`);
+-- ALTER TABLE messages ADD FOREIGN KEY (roomnameid) REFERENCES `rooms`(`id`);
+
+-- FOR FOREIGN KEYS: https://www.rathishkumar.in/2016/01/solved-how-to-solve-mysql-error-code.html
 
 -- CREATE TABLE userroom (
 -- FOREIGN KEY (usernameid) REFERENCES roomname(id),
